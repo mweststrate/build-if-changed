be689863b68e67ecd67ef63c416614efc96f8955
Michel Weststrate <mweststrate@gmail.com>
2015-11-18 22:44:42 +0100

    Initial commit, example is already working :)

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # build-if-changed
-Minimal build system for maximal efficience
+
+Minimal build system for maximal efficiency
+
+## Why `build-if-changed`?
+
+`build-if-changed` is like a portable version of `make` but way more simple.
+given a bunch of file patterns it executes a command if (and only if) any of the files inside the patterns did change.
+A simple pattern that allows for powerful composition for dependent build tasks.
+
+## Installation
+
+`npm install -g build-if-changed`
+
+## General usage
+
+Run `build-if-changed` in any directory. It will search in the current directory or upwards for file named `buildconfig`.
+
+A build config file looks like this:
+
+```
+[some command]
+glob pattern1 to watch
+glob pattern2 to watch
+
+[another command]
+more patterns to watch
+```
+
+`build-if-changed` will calculate the hashes of all files that match the patterns,
+and execute the matching command(s) if the files did change since the previous run of `build-if-changed`.
+It is perfectly fine if the output of one command is watched by another command, `build-if-changed` will keep trying to run commands until no files change anymore.  
+
+Glob patterns and patterns are always interpreted relatively to the location of the `buildconfig` file.
+
+Also see the [examples](examples/) directory for some examples.
+
+## Command usage
+
+Command syntax is: `build-if-changed [--watch] [-c directory] [--clean] [file1] [file2]
+
+#### --watch
+
+(TODO)
+Same as `build-if-changed`, but, as suggested, will keep watching the files for any future changes.
+The watch will continue to run even if some commands did fail.
+
+#### -c directory
+
+(TODO)
+Run's `build-if-changed` in the specified directory, searching for a `buildconfig` file in that directory and upward.
+
+#### file1 file2 ...
+
+(TODO)
+Reads configuration from the specified configuration file(s).
+Note that the configuration files are still interpreted relatively to their own location. 
+
+#### --clean
+
+(TODO)
+Drops the `.buildifchanged` directory and thereby forcing all commands to run upon the next invocation of `build-if-changed`.
+
+## Questions
+
+**Q: What is this magically appearing `.buildifchanged` folder?**
+A:File hashes as stored in the folder `.buildifchanged` in the same directory as the `buildconfig` file. This file should be excluded from version control.
+
+**Q: Why can a build command be only one line?**
+A: Separation of concerns;
+build-if-changed only determines when your build tools should be run. 
+Complex commands should be organized outside build-if-changed so that you can test, version and invoke them manually.
+Use any tool you are comfortable with to organize your build scripts, `sh`, `npm`, `gulp`, `webpack`....  
---
+++ index.js
@@ -0,0 +1,204 @@
+#!/usr/bin/env node
+/**
+ * build-if-changed
+ * Minimalistic build tools
+ * (c) Michel Weststrate, 2015
+ */
+
+/**
+ * TODO:
+ * add command line parser
+ * support -c (in directory option)
+ * support watch mode
+ * support one (or more build) file configs as input argument
+ * support cleanup command
+ */
+
+var process = require('process');
+var fs = require('fs');
+var path = require('path');
+var globby = require('globby');
+var crypto = require('crypto');
+var checksum = require('checksum');
+var child_process = require('child_process');
+var chalk = require('chalk');
+
+var DEFAULT_BUILDFILE = 'buildconfig';
+var CACHE_DIR = '/.buildifchanged';
+var LOG_PREFIX = "[build if changed] ";
+
+function main() {
+	var configFileName = findConfigFile();
+	if (!configFileName) {
+		exit("No '" + DEFAULT_BUILDFILE + "' file found in " + process.cwd() + " or one of it's parent directories", 1);
+	}
+
+	var basePath = path.dirname(configFileName);
+	log("Using config '" + path.basename(configFileName) + "' in '" + basePath + "'");
+	var tasks = readConfigFile(configFileName);
+	if (!fs.existsSync(basePath + CACHE_DIR))
+		fs.mkdirSync(basePath + CACHE_DIR);
+	if (!tasks.length) {
+		exit("The build configuration file is empty", 3);
+	}
+	runTasksUntilExhausted(basePath, tasks);
+}
+
+function findConfigFile() {
+	var currentDir = process.cwd();
+	while(path.resolve(currentDir) !== path.resolve(currentDir + '/..')) {
+		currentDir = path.resolve(currentDir);
+		if (fs.existsSync(currentDir + '/' + DEFAULT_BUILDFILE))
+			return path.resolve(currentDir + '/' + DEFAULT_BUILDFILE);
+		currentDir += '/..';
+	}
+	return null;
+}
+
+function readConfigFile(filename) {
+	var tasks = [];
+	var currentTask;
+	fs.readFileSync(filename, 'utf8').split('\n').forEach(function(line) {
+		// New task definition
+		if (line.charAt(0) === '[') {
+			if (currentTask && currentTask.patterns.length === 0) {
+				exit("command '" + currentTask.cmd + "' didn't specifiy any dependencies", 6);
+			}
+			currentTask = {
+				cmd: line.replace(/^\s*\[/,'').replace(/]\s*$/,''),
+				patterns: []
+			}
+			if (!currentTask.cmd)
+				exit('task command should not be empty');
+			currentTask.cmdMd5 = stringToMd5(currentTask.cmd);
+			if (tasks.filter(
+					function(task) {
+						return task.cmdMd5 === currentTask.cmdMd5
+					}
+				).length > 0) {
+				exit('task command should be unique')
+			}
+			tasks.push(currentTask);
+		} else if (line.charAt(0) === '#') {
+			// noop, comment
+		} else {
+			if (!currentTask)
+				exit('buildconfig files should start with a shell command between brackets. E.g.: \n[sass *.js -o main.css]\n**/*.scss\n...more dependencies', 2);
+			currentTask.patterns.push(line);
+		}
+	});
+	return tasks;
+}
+
+function runTasksUntilExhausted(path, tasks) {
+	var didSomeTaskRunInLastIteration = false;
+	var didAnyTaskRun = false;
+	var i = -1;
+	function runNextTask(err, taskDidRun) {
+		if (err) {
+			console.err(err);
+			exit("Encountered errors during run, exiting", 5);
+		}
+		if (taskDidRun) {
+			didAnyTaskRun = didSomeTaskRunInLastIteration = true;
+		}
+		i += 1;
+		if (i >= tasks.length) {
+			if (didSomeTaskRunInLastIteration) {
+				i = 0;
+				didSomeTaskRunInLastIteration = false;
+			} else {
+				exit(didAnyTaskRun ? "FINISHED, some tasks did run" : "SKIPPED, no changes detected");
+			}
+		}
+		runTask(path, tasks[i], runNextTask);
+	}
+
+	runNextTask(null, false);
+}
+
+function runTask(path, task, callback) {
+	globby(task.patterns, {
+		cwd: path,
+		nodir: true
+	}).then(function(files) {
+		 var left = files.length;
+		 var md5s = new Array(left);
+		 files.forEach(function(filename, idx) {
+			checksum.file(path + '/' + filename, function(err, sha) {
+				if (err)
+					return void callback(err);
+				md5s[idx] = sha;
+				if (--left === 0) {
+					runTaskIfHashesModified(path, task, md5s.map(function(md5, idx) {
+						return md5 + ' ' + files[idx];
+					}).join('\n'), callback);
+				}
+			});
+		});
+	}, callback);
+}
+
+function runTaskIfHashesModified(path, task, hashData, callback) {
+	var hashesFile = path + '/' + CACHE_DIR + '/' + task.cmdMd5 + '-hashes';
+	var existingHashes = fs.existsSync(hashesFile) ? fs.readFileSync(hashesFile, 'utf8') : '';
+	if (existingHashes !== hashData) {
+		executeTask(path, task, function(err) {
+			if (err)
+				return void callback(err);
+			fs.writeFileSync(hashesFile, hashData, 'utf8');
+			callback(null, true);
+		});
+	} else {
+		callback();
+	}
+}
+
+function executeTask(path, task, callback) {
+	var command = task.cmd;
+	var file, args;
+	var options = {
+		cwd: path,
+		stdio: 'inherit'
+	};
+
+	// From: https://github.com/nodejs/node/blob/master/lib/child_process.js#L73
+	if (process.platform === 'win32') {
+		file = process.env.comspec || 'cmd.exe';
+		args = ['/s', '/c', '"' + command + '"'];
+		options.windowsVerbatimArguments = true;
+	} else {
+		file = '/bin/sh';
+		args = ['-c', command];
+	}
+
+	log(chalk.dim("Starting:") + " " + chalk.bold(command));
+	child_process.spawn(file, args, options)
+		.on('exit', function(code, signal) {
+			if (code === null)
+				exit("task exited prematurely with " + signal  + " (task: '" + command + "')", 13);
+			if (code !== 0)
+				exit("task failed with exit code " + code + " (task: '" + command + "')", code);
+			log(chalk.dim("Finished:") + " " + chalk.bold(command));
+			callback();
+		});
+}
+
+function stringToMd5(value) {
+	return crypto.createHash('md5').update(value).digest('hex');
+}
+
+function exit(msg, code) {
+	code = code || 0;
+	if (code === 0)
+		log(msg);
+	else
+		console.error(chalk.bold.red(LOG_PREFIX + "[error] " + msg));
+	process.exit(code);
+}
+
+function log(msg) {
+	console.log(chalk.cyan(LOG_PREFIX) + msg);
+}
+
+main();
