360fe740e6623b22875abe0d2a7251475aa5097a
Michel Weststrate <mweststrate@gmail.com>
2015-11-19 02:48:48 +0100

    output files are now checked as well (just not cleaned yet)

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
 /**
  * TODO:
+ * add output specification
  * add command line parser
  * support -c (in directory option)
  * support watch mode
@@ -31,7 +32,7 @@ var LOG_PREFIX = "[build if changed] ";
 function main() {
 	var configFileName = findConfigFile();
 	if (!configFileName) {
-		exit("No '" + DEFAULT_BUILDFILE + "' file found in " + process.cwd() + " or one of it's parent directories", 1);
+		return void exit("No '" + DEFAULT_BUILDFILE + "' file found in " + process.cwd() + " or one of it's parent directories", 1);
 	}
 
 	var basePath = path.dirname(configFileName);
@@ -40,7 +41,7 @@ function main() {
 	if (!fs.existsSync(basePath + CACHE_DIR))
 		fs.mkdirSync(basePath + CACHE_DIR);
 	if (!tasks.length) {
-		exit("The build configuration file is empty", 3);
+		return void exit("The build configuration file is empty", 3);
 	}
 	runTasksUntilExhausted(basePath, tasks);
 }
@@ -62,30 +63,33 @@ function readConfigFile(filename) {
 	fs.readFileSync(filename, 'utf8').split('\n').forEach(function(line) {
 		// New task definition
 		if (line.charAt(0) === '[') {
-			if (currentTask && currentTask.patterns.length === 0) {
-				exit("command '" + currentTask.cmd + "' didn't specifiy any dependencies", 6);
+			if (currentTask && currentTask.inputPatterns.length === 0) {
+				return void exit("command '" + currentTask.cmd + "' didn't specifiy any dependencies", 6);
 			}
 			currentTask = {
 				cmd: line.replace(/^\s*\[/,'').replace(/]\s*$/,''),
-				patterns: []
+				inputPatterns: [],
+				outputPatterns: []
 			}
 			if (!currentTask.cmd)
-				exit('task command should not be empty');
+				return void exit('task command should not be empty');
 			currentTask.cmdMd5 = stringToMd5(currentTask.cmd);
 			if (tasks.filter(
 					function(task) {
 						return task.cmdMd5 === currentTask.cmdMd5
 					}
 				).length > 0) {
-				exit('task command should be unique')
+				return void exit('task command should be unique')
 			}
 			tasks.push(currentTask);
 		} else if (line.charAt(0) === '#') {
 			// noop, comment
 		} else if (line) {
 			if (!currentTask)
-				exit('buildconfig files should start with a shell command between brackets. E.g.: \n[sass *.js -o main.css]\n**/*.scss\n...more dependencies', 2);
-			currentTask.patterns.push(line);
+				return void exit('buildconfig files should start with a shell command between brackets. E.g.: \n[sass *.js -o main.css]\n**/*.scss\n...more dependencies', 2);
+			if (line.indexOf('out:') === 0)
+				currentTask.outputPatterns.push(line.substr(4));
+			currentTask.inputPatterns.push(line);
 		}
 	});
 	return tasks;
@@ -93,15 +97,16 @@ function readConfigFile(filename) {
 
 function runTasksUntilExhausted(path, tasks) {
 	var didSomeTaskRunInLastIteration = false;
-	var didAnyTaskRun = false;
+	var amountOfTaskRun = 0;
 	var i = -1;
 	function runNextTask(err, taskDidRun) {
 		if (err) {
 			console.err(err);
-			exit("Encountered errors during run, exiting", 5);
+			return void exit("Encountered errors during run, exiting", 5);
 		}
 		if (taskDidRun) {
-			didAnyTaskRun = didSomeTaskRunInLastIteration = true;
+			amountOfTaskRun += 1;
+			didSomeTaskRunInLastIteration = true;
 		}
 		i += 1;
 		if (i >= tasks.length) {
@@ -109,7 +114,7 @@ function runTasksUntilExhausted(path, tasks) {
 				i = 0;
 				didSomeTaskRunInLastIteration = false;
 			} else {
-				exit(didAnyTaskRun ? "FINISHED, some tasks did run" : "SKIPPED, no changes since last run");
+				return void exit(amountOfTaskRun > 0 ? "FINISHED, " + amountOfTaskRun + " task(s) completed" : "SKIPPED, no changes found");
 			}
 		}
 		runTask(path, tasks[i], runNextTask);
@@ -119,51 +124,65 @@ function runTasksUntilExhausted(path, tasks) {
 }
 
 function runTask(path, task, callback) {
-	globby(task.patterns, {
-		cwd: path,
-		nodir: true
-	}).then(function(files) {
-		var left = files.length;
-		var md5s = new Array(left);
-
-		function gotHashes() {
-			runTaskIfHashesModified(path, task, md5s.map(function(md5, idx) {
-				return md5 + ' ' + files[idx];
-			}).join('\n'), callback);
-		}
-
-		if (files.length === 0) {
-			gotHashes();
-		} else {
-			files.forEach(function(filename, idx) {
-				checksum.file(path + '/' + filename, function(err, sha) {
-					if (err)
-						return void callback(err);
-					md5s[idx] = sha;
-					if (--left === 0)
-						gotHashes();
-				});
-			});
-		}
-	}, callback);
+	// optimization: it isn't necessary to get the output hashes if the input changed anyway..
+	getHashesFromGlobs(path, task.outputPatterns, function(outputErr, outputFilesHashData) {
+		getHashesFromGlobs(path, task.inputPatterns, function(inputErr, inputFilesHashData) {
+			if (outputErr || inputErr)
+				return void callback(outputErr || inputErr);
+			runTaskIfHashesModified(path, task, outputFilesHashData, inputFilesHashData, callback);
+		});
+	});
 }
 
-function runTaskIfHashesModified(path, task, hashData, callback) {
-	var hashesFile = path + '/' + CACHE_DIR + '/' + task.cmdMd5 + '-hashes';
-	var existingHashes = fs.existsSync(hashesFile) ? fs.readFileSync(hashesFile, 'utf8') : '';
-	if (existingHashes !== hashData) {
-		executeTask(path, task, function(err) {
-			if (err)
-				return void callback(err);
-			fs.writeFileSync(hashesFile, hashData, 'utf8');
-			callback(null, true);
-		});
+function runTaskIfHashesModified(path, task, outputFilesHashData, inputFilesHashData, callback) {
+	var outputHashesFile = path + '/' + CACHE_DIR + '/' + task.cmdMd5 + '-out-hashes';
+	var inputHashesFile  = path + '/' + CACHE_DIR + '/' + task.cmdMd5 + '-in-hashes';
+
+	var existingInputHashes = fs.existsSync(inputHashesFile) ? fs.readFileSync(inputHashesFile, 'utf8') : '';
+	var existingOutputHashes = fs.existsSync(outputHashesFile) ? fs.readFileSync(outputHashesFile, 'utf8') : '';
+
+	var shouldBuild = true;
+	var buildReason;
+
+	if (!outputFilesHashData && task.outputPatterns.length) {
+		buildReason = "(output files are missing)";
+	} else if (existingOutputHashes !== outputFilesHashData) {
+		buildReason = "(output files have changed)"
+	} else if (existingInputHashes !== inputFilesHashData) {
+		buildReason = "(input files have changed)";
+	} else {
+		shouldBuild = false;
+	}
+	
+	if (shouldBuild) {
+		// clean output files!
+		globby(task.outputPatterns, {
+			cwd: path,
+			nodir: true
+		}).then(function(files) {
+			// TODO: delete those files
+			
+			// finally, build!
+			executeTask(path, task, buildReason, function(err) {
+				if (err)
+					return void callback(err);
+				getHashesFromGlobs(path, task.outputPatterns, function(err, freshOutputHashData) {
+					if (err)
+						return void callback(err);
+					if (task.outputPatterns.length && !freshOutputHashData)
+						return void exit("Executing task '" + task.cmd + "' didn't result in any files being written on disk! Patterns: " + task.outputPatterns.join(", "));
+					fs.writeFileSync(outputHashesFile, freshOutputHashData, 'utf8');
+					fs.writeFileSync(inputHashesFile,  inputFilesHashData,  'utf8');
+					callback(null, true);
+				});
+			});
+		}, callback);
 	} else {
 		callback();
 	}
 }
 
-function executeTask(path, task, callback) {
+function executeTask(path, task, buildReason, callback) {
 	var command = task.cmd;
 	var file, args;
 	var options = {
@@ -181,16 +200,47 @@ function executeTask(path, task, callback) {
 		args = ['-c', command];
 	}
 
-	log(chalk.dim("Starting:") + " " + chalk.bold(command));
+	log(chalk.dim("Starting:") + " " + chalk.bold(command) + " " + chalk.dim(buildReason));
 	child_process.spawn(file, args, options)
 		.on('exit', function(code, signal) {
 			if (code === null)
-				exit("task exited prematurely with " + signal  + " (task: '" + command + "')", 13);
+				return void exit("task exited prematurely with " + signal  + " (task: '" + command + "')", 13);
 			if (code !== 0)
-				exit("task failed with exit code " + code + " (task: '" + command + "')", code);
+				return void exit("task failed with exit code " + code + " (task: '" + command + "')", code);
 			log(chalk.dim("Finished:") + " " + chalk.bold(command));
 			callback();
 		});
+}
+
+function getHashesFromGlobs(path, patterns, callback) {
+	// Obtain and verify output hashes first
+	globby(patterns, {
+		cwd: path,
+		nodir: true
+	}).then(function(files) {
+		var left = files.length;
+		var md5s = new Array(left);
+
+		function gotHashes() {
+			callback(null, md5s.map(function(md5, idx) {
+				return md5 + ' ' + files[idx];
+			}).join('\n'));
+		}
+
+		if (files.length === 0) {
+			gotHashes();
+		} else {
+			files.forEach(function(filename, idx) {
+				checksum.file(path + '/' + filename, function(err, sha) {
+					if (err)
+						return void callback(err);
+					md5s[idx] = sha;
+					if (--left === 0)
+						gotHashes();
+				});
+			});
+		}
+	}, callback);	
 }
 
 function stringToMd5(value) {
